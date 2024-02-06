17c9cc5fe42c1076cdc193ad9921afef4bf74d97
Dipesh Mishra <mavinash422@gmail.com>
2024-02-06 21:42:50 +0530

    Contains svg animation(First commit)

--- script.js
+++
@@ -1,9 +0,0 @@
-function sendMail(){
-    let parms={
-        name:"Dipesh",
-        email:"yraikar22it@student.mes.ac.in",
-        subject:"test",
-        message:"test"
-    }
-    emailjs.send("service_p1h9ans","template_y2h48zh",parms).then(alert("Email sent!"))
-}
